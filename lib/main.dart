f2a81cf42189bbd95b12579b3809dd366b7f168d
Arwa <arwaqaddour92@gmail.com>
2023-08-09 21:48:30 +0300

    show_date_time_app_using_intl

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,122 +28,16 @@ class DateApp extends StatefulWidget {
 }
 
 class _DateAppState extends State<DateApp> {
-//Times
-  int hour = 0;
-  String min = "";
-  String sec = "";
-
-//Dates
-  String year = "";
-  String month = "";
-  String dayNum = "";
   String weekday = "";
-  String amORpm = "";
+  String date = "";
+  String timeNow = "";
 
   ChangeEverySecond() {
     Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
-//Time
-        hour = DateTime.now().hour;
-        if (hour > 12) {
-          hour = hour - 12;
-          amORpm = "pm";
-        }
-        min = DateTime.now().minute.toString();
-        sec = DateTime.now().second.toString();
-
-        //Dates
-        year = DateTime.now().year.toString();
-        month = DateTime.now().month.toString();
-        switch (month) {
-          case "1":
-            month = "January";
-            break;
-
-          case "2":
-            month = "February";
-            break;
-
-          case "3":
-            month = "March";
-            break;
-
-          case "4":
-            month = "April";
-            break;
-
-          case "5":
-            month = "May";
-            break;
-
-          case "6":
-            month = "June";
-            break;
-
-          case "7":
-            month = "July";
-            break;
-
-          case "8":
-            month = "August";
-            break;
-
-          case "9":
-            month = "September";
-            break;
-
-          case "10":
-            month = "October";
-            break;
-
-          case "11":
-            month = "November";
-            break;
-
-          case "12":
-            month = "December";
-            break;
-
-          default:
-        }
-
-        dayNum = DateTime.now().day.toString();
-        weekday = DateTime.now().weekday.toString();
-        switch (weekday) {
-          case "1":
-            weekday = "Monday";
-            break;
-
-          case "2":
-            weekday = "Tuesday";
-            break;
-          case "3":
-            weekday = "Wednesday";
-            break;
-          case "4":
-            weekday = "Thersday";
-            break;
-          case "5":
-            weekday = "Friday";
-            break;
-          case "6":
-            weekday = "Saturday";
-            break;
-          case "7":
-            weekday = "Sunday";
-            break;
-
-          default:
-        }
-      });
-    });
-  }
-
-  String qqq = "";
-  calledOnlyTimerAfter7sec() {
-    Timer(Duration(seconds: 5), () {
-      setState(() {
-        qqq = "Welcome";
+        weekday = DateFormat("EEEE").format(DateTime.now());
+        date = DateFormat("MMMM d , y").format(DateTime.now());
+        timeNow = DateFormat('hh :mm : ss a').format(DateTime.now());
       });
     });
   }
@@ -152,7 +47,6 @@ class _DateAppState extends State<DateApp> {
     // TODO: implement initState
     super.initState();
     ChangeEverySecond();
-    calledOnlyTimerAfter7sec();
   }
 
   @override
@@ -175,14 +69,9 @@ class _DateAppState extends State<DateApp> {
               style: TextStyle(fontSize: 27, color: Colors.white),
             ),
             SizedBox(height: 22),
-            Text("$month $dayNum , $year",
-                style: TextStyle(fontSize: 27, color: Colors.white)),
+            Text(date, style: TextStyle(fontSize: 27, color: Colors.white)),
             SizedBox(height: 22),
-            Text(
-                " ${hour.toString().padLeft(2, "0")}: ${min.padLeft(2, "0")} : ${sec.padLeft(2, "0")}, $amORpm",
-                style: TextStyle(fontSize: 27, color: Colors.white)),
-            SizedBox(height: 22),
-            Text(qqq, style: TextStyle(fontSize: 27, color: Colors.white)),
+            Text(timeNow, style: TextStyle(fontSize: 27, color: Colors.white)),
           ],
         ),
       ),
